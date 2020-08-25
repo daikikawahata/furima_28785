3ac241f6cfb2fb22505537f021b17577b57d9c17
daikikawahata <daikikawahata@icloud.com>
2020-08-25 17:16:13 +0900

    userモデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])\w{6}\z/
+
+  with_options presence: true do
+    validates :password, format: { with: VALID_PASSWORD_REGEX }
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :family_name_furigana, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :first_name_furigana, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :birth_day
+    validates :email
+    validates :nickname
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_055610) do
+ActiveRecord::Schema.define(version: 2020_08_24_020134) do
 
   create_table "buyers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -52,18 +52,18 @@ ActiveRecord::Schema.define(version: 2020_08_24_055610) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "nickname", null: false
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_furigana", null: false
+    t.string "first_name_furigana", null: false
+    t.date "birth_day", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "nickname"
-    t.string "family_name"
-    t.string "first_name"
-    t.string "family_name_furigana"
-    t.string "first_name_furigana"
-    t.date "birth_day"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
