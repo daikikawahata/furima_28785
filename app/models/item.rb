057d88e035957ccf52d047fec398b029f54f85f6
daikikawahata <daikikawahata@icloud.com>
2020-08-26 12:00:39 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one :buyer
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :pay_postage
-  belongs_to_active_hash :shipping_data
+  belongs_to_active_hash :shipping_date
   belongs_to_active_hash :prefecture
+
+  validates :image, :category, :name, :explanation, :price, :condition, :pay_postage, :prefecture, :shipping_date, presence: true
+
+  validates :category_id, :condition_id, :pay_postage_id, :shipping_date_id, numericality: { other_than: 1 }
+  validates  :prefecture_id, numericality: { other_than: 0 }
 end
